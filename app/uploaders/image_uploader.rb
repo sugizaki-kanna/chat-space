bd58569ef19aaa5e0f520e0fc674ca3b3537b07c
sugizaki-kanna <c.k._02@icloud.com>
2020-02-17 17:01:16 +0900

    user_group_function.2

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   process resize_to_fit: [800, 800]
   # storage :fog
-
+end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
