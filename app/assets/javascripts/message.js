27ae3213d63ff5016e70ce2a87b44baab6b20d46
sugizaki-kanna <c.k._02@icloud.com>
2020-03-15 14:40:24 +0900

    Update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,71 +1,70 @@
 $(function(){ 
   last_message_id = $('.message:last').data("message-id");
   console.log(last_message_id);
-function buildHTML(message){
-if ( message.image ) {
-  var html =
-    `<div class="message" data-message-id=${message.id}>
-      <div class="upper-message">
-        <div class="upper-message__user-name">
-          ${message.user_name}
-        </div>
-        <div class="upper-message__date">
-          ${message.created_at}
-        </div>
-      </div>
-      <div class="lower-message">
-        <p class="lower-message__content">
-          ${message.content}
-        </p>
-      </div>
-      <img src=${message.image} >
-    </div>`
-  return html;
-} else {
-  var html =
-    `<div class="message" data-message-id=${message.id}>
-      <div class="upper-message">
-        <div class="upper-message__user-name">
-          ${message.user_name}
-        </div>
-        <div class="upper-message__date">
-          ${message.created_at}
-        </div>
-      </div>
-      <div class="lower-message">
-        <p class="lower-message__content">
-          ${message.content}
-        </p>
-      </div>
-    </div>`
-  return html;
-};
-}
-$('#new_message').on('submit', function(e){
-e.preventDefault();
-var formData = new FormData(this);
-var url = $(this).attr('action')
-$.ajax({
-url: url,
-type: "POST",
-data: formData,
-dataType: 'json',
-processData: false,
-contentType: false
-})
-.done(function(data){
-    var html = buildHTML(data);
-    $('.chat-main__messages').append(html);      
-    $('form')[0].reset();
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__messages').append(html);      
+      $('form')[0].reset();
 
-    $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
-    $('.chat-main__form__submit').prop('disabled', false);
-  });
+      $('.chat-main__messages').animate({ scrollTop: $('.chat-main__messages')[0].scrollHeight});
+      $('.chat-main__form__submit').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   })
-  .fail(function() {
-    alert("メッセージ送信に失敗しました");
-});
-  
 })
 
 
@@ -102,5 +101,3 @@ var reloadMessages = function() {
 if (document.location.href.match(/\/groups\/\d+\/messages/)) {
   setInterval(reloadMessages, 1000);
 }
-
-});
